1ca4c777ecbfa09d0d9b6712bbcdf316d1db9dae
kapil-infobeans <kapil.yadav@infobeans.com>
2023-01-05 11:12:30 +0530

    Improvements

--- resources/views/adminhome.blade.php
+++ resources/views/adminhome.blade.php
@@ -153,4 +153,20 @@
       </div>
    </div>
 </div>
+<!-- <div class="row">
+   <div class="col-xl-8 col-lg-8">
+      <div class="card bg-dark text-white">
+      <img src="https://mdbcdn.b-cdn.net/img/new/slides/017.webp" class="card-img" alt="Stony Beach"/>
+      <div class="card-img-overlay">
+         <h5 class="card-title">Card title</h5>
+         <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in to additional
+            content. This content is a little bit longer.
+         </p>
+         <p class="card-text">Last updated 3 mins ago</p>
+         <button type="button" class="btn btn-primary">Button</button>
+      </div>
+      </div>
+   </div>
+</div> -->
 @endsection -->
